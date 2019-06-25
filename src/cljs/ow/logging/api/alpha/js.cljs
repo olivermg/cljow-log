654530d3d88c1ba392b814a44c26e6451004b625
Oliver Wegner <void1976@gmail.com>
2019-06-25 11:35:29 +0200

    completing js interop layer

--- src/cljs/ow/logging/api/alpha/js.cljs
+++ src/cljs/ow/logging/api/alpha/js.cljs
@@ -10,6 +10,18 @@
   (am/with-data (js->clj data)
     (cb)))
 
+(defn ^:export get-checkpoints []
+  (clj->js (a/get-checkpoints)))
+
+(defn ^:export get-root-checkpoint []
+  (clj->js (a/get-root-checkpoint)))
+
+(defn ^:export log-data [level msg & [data]]
+  (clj->js (a/log-data (keyword level) msg (js->clj data))))
+
+(defn ^:export log [level msg & [data]]
+  (a/log (keyword level) msg (js->clj data)))
+
 (defn ^:export trace [msg & [data]]
   (a/trace msg (js->clj data)))
 
@@ -27,3 +39,13 @@
 
 (defn ^:export fatal [msg & [data]]
   (a/fatal msg (js->clj data)))
+
+(defn ^:export attach [obj]
+  (a/attach obj))
+
+(defn ^:export detach [obj]
+  (a/detach obj))
+
+(defn ^:export with-historical-logging-info [logging-info cb]
+  (am/with-historical-logging-info logging-info
+    (cb)))
