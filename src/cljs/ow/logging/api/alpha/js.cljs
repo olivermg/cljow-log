d802fe164d720ba73c576a91e21e175deee1c329
Oliver Wegner <void1976@gmail.com>
2019-07-01 14:23:11 +0200

    use json to convert js objects into logable format and back to cljs

--- src/cljs/ow/logging/api/alpha/js.cljs
+++ src/cljs/ow/logging/api/alpha/js.cljs
@@ -2,17 +2,19 @@
   (:require-macros [ow.logging.api.alpha :as am])
   (:require [ow.logging.api.alpha :as a]))
 
-(defn- stringify [v]
-  (cond
-    (str? v) v
-    true     (.stringify js/JSON v)))
+(defn- js->clj* [v]
+  (->> v
+       clj->js
+       (.stringify js/JSON)
+       (.parse js/JSON)
+       js->clj))
 
 (defn ^:export with-checkpoint [name cb]
   (am/with-checkpoint name
     (cb)))
 
 (defn ^:export with-data [data cb]
-  (am/with-data (stringify data)
+  (am/with-data (js->clj* data)
     (cb)))
 
 (defn ^:export get-checkpoints []
@@ -22,28 +24,28 @@
   (clj->js (a/get-root-checkpoint)))
 
 (defn ^:export log-data [level msg & [data]]
-  (clj->js (a/log-data (keyword level) msg (stringify data))))
+  (clj->js (a/log-data (keyword level) msg (js->clj* data))))
 
 (defn ^:export log [level msg & [data]]
-  (a/log (keyword level) msg (stringify data)))
+  (a/log (keyword level) msg (js->clj* data)))
 
 (defn ^:export trace [msg & [data]]
-  (a/trace msg (stringify data)))
+  (a/trace msg (js->clj* data)))
 
 (defn ^:export debug [msg & [data]]
-  (a/debug msg (stringify data)))
+  (a/debug msg (js->clj* data)))
 
 (defn ^:export info [msg & [data]]
-  (a/info msg (stringify data)))
+  (a/info msg (js->clj* data)))
 
 (defn ^:export warn [msg & [data]]
-  (a/warn msg (stringify data)))
+  (a/warn msg (js->clj* data)))
 
 (defn ^:export error [msg & [data]]
-  (a/error msg (stringify data)))
+  (a/error msg (js->clj* data)))
 
 (defn ^:export fatal [msg & [data]]
-  (a/fatal msg (stringify data)))
+  (a/fatal msg (js->clj* data)))
 
 (defn ^:export attach [obj]
   (a/attach obj))
